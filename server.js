afe35ff5c897e3676727bcd31aa385550e0ad3fa
Jeff Woda <wodahouse@gmail.com>
2021-02-13 12:57:02 -0500

    initial layout files

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-var potato = "stuff";
+const express = require('express');
 
-var fries = "stuff";
+const PORT = process.env.PORT || 8080;
 
-var bigPotato = "more stuff";
+const app = express();
 
-var ILikeToast = "notPotato";
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
 
-var michael = "mydad";
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+const routes = require('./controllers/catsController.js');
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
