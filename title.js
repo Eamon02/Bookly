78fbd5ad0ad35aa8af57c142f8f8d7d6f7dd855a
Eamon McHugh <eamonmchugh2@gmail.com>
2021-02-16 20:54:48 -0500

    background and title animation'

---
+++ title.js
@@ -0,0 +1,9 @@
+anime({
+    targets: '#title path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 5000,
+    delay: function(el, i) { return i * 200 },
+    direction: 'alternate',
+    loop: true
+  });
