336246cb9f47be50eab733a0d772d5c0dd4fa88d
Jeff Woda <wodahouse@gmail.com>
2021-02-16 20:52:34 -0500

    updating mvc structure removing controller path

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,2 @@
--- ### Schema
-
--- CREATE DATABASE cat_db;
--- USE cat_db;
-
--- CREATE TABLE cats
--- (
--- 	id int NOT NULL AUTO_INCREMENT,
--- 	name varchar(255) NOT NULL,
--- 	sleepy BOOLEAN DEFAULT false,
--- 	PRIMARY KEY (id)
--- );
-
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS bookly;
--- Creates the "blogger" database --
-CREATE DATABASE bookly;
+DROP DATABASE IF EXISTS bookly_db;
+CREATE DATABASE bookly_db;
