9ecdfd736834452b03ec1bcefe0e96fd4dc9ef5d
Jeff Woda <wodahouse@gmail.com>
2021-02-18 20:45:10 -0500

    fixed seeds and database

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,6 +4,29 @@ var db = require("../models");
 
 module.exports = function(app) {
 
+    app.get('/api/authors', (req, res) => {
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Post
+        db.Author.findAll({
+          include: [db.Post],
+        }).then((dbAuthor) => res.json(dbAuthor));
+      });
+
+      app.get('/api/book/:title', (req, res) => {
+        
+        db.Author.findOne({
+          where: {
+            title: req.params.title,
+          },
+          include: [db.Post],
+        }).then((dbAuthor) => res.json(dbAuthor));
+      });
+
+
+
+
+
+
 }
 
-///
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,25 +4,28 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", (req, res) => {
-   
-    res.render("index")
+     res.render("index")
   })
+
+  app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/blog.html'))
+);
+
+app.get('/books', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/cms.html'))
+);
+
+app.get('/authors', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/author-manager.html'))
+);
+
+
+
+
+
+
+
+
+
 }
 
-  
-
-//   app.get("/login", function(req, res) {
-//     // If the user already has an account send them to the members page
-//     if (req.user) {
-//       res.redirect("/members");
-//     }
-//     res.sendFile(path.join(__dirname, "../public/login.html"));
-//   });
-
-//   // Here we've add our isAuthenticated middleware to this route.
-//   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-//   app.get("/members", isAuthenticated, function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/members.html"));
-//   });
-
-// };
