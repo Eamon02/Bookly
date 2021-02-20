a0a84e04933abdb90183177915e5d7ca19f2a8db
Eamon McHugh <eamonmchugh2@gmail.com>
2021-02-19 20:46:52 -0500

    db loading

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,11 +2,4 @@ use bookly_db;
 
 INSERT INTO booktable (title , author, user_rating, reading_status, category) 
 VALUES 
-    ("Harry Harry Potter and the Sorcerer's", "J.K. Rowling", "Awesome", "Read", "Fantasy"),
-    ("Harry Potter and the Chamber of Secrets", "J.K.Rowling", "Awesome", "Read", "Fantasy"),
-    ("Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", "Awesome", "Reading", "Fantasy"),
-    ("Harry Potter and the Goblet of Fire", "J.K.Rowling", "Not Yet Rated", "On List", "Fantasy"),
-    ("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "Not Yet Rated", "On List", "Fantasy"),
-    ("Complete Book of Classic John Deere Tractors", "Don Mcmillan", "Boring", "Read", "Classic"),
-    ("Eight Perfect Murders", "Peter", "Swanson", "Not Yet Rated", "On List", "Mystery");
-
+    ("Harry Harry Potter and the Sorcerer's", "J.K. Rowling", "Awesome", "Read", "Fantasy")
