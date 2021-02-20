5c2eabe77d5e3f1fdc28a00b6edb349102c28b38
Jeff Woda <wodahouse@gmail.com>
2021-02-19 20:55:56 -0500

    updating database structure and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,12 @@
 use bookly_db;
 
-INSERT INTO booktable (title , author, user_rating, reading_status, category) 
+INSERT INTO booktables (title, author, user_rating, reading_status, category, createdAt, updatedAt) 
 VALUES 
-    ("Harry Harry Potter and the Sorcerer's", "J.K. Rowling", "Awesome", "Read", "Fantasy"),
-    ("Harry Potter and the Chamber of Secrets", "J.K.Rowling", "Awesome", "Read", "Fantasy"),
-    ("Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", "Awesome", "Reading", "Fantasy"),
-    ("Harry Potter and the Goblet of Fire", "J.K.Rowling", "Not Yet Rated", "On List", "Fantasy"),
-    ("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "Not Yet Rated", "On List", "Fantasy"),
-    ("Complete Book of Classic John Deere Tractors", "Don Mcmillan", "Boring", "Read", "Classic"),
-    ("Eight Perfect Murders", "Peter", "Swanson", "Not Yet Rated", "On List", "Mystery");
+    ("Harry Potter and the Sorcerer's", "J.K. Rowling", "Awesome", "Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Harry Potter and the Chamber of Secrets", "J.K. Rowling", "Awesome", "Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "Awesome", "Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Harry Potter and the Goblet of Fire", "J.K. Rowling", "Not Yet Rated", "On List", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Complete Book of Classic John Deere Tractors", "Don Mcmillan", "Boring", "Read", "Classic","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Eight Perfect Murders", "Peter Swanson", "Not Yet Rated", "On List", "Mystery","2020-05-07 15:41:58","2020-05-07 15:41:58");
+
 
