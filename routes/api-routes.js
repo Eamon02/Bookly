d8bda0f2ffcd3a675618e855b36939bb8da5edaf
amo02008 <adrienne.osorio@gmail.com>
2021-02-20 11:07:26 -0500

    not sure just trying to merge

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,6 @@ module.exports = function (app) {
       })
       .then((booktitle) => res.json(booktitle));
   });
-<<<<<<< HEAD
 };
 
 // app.post('/api/books', (req, res) => {
@@ -35,8 +34,6 @@ module.exports = function (app) {
 //     },
 //   }).then((dbbooktable) => res.json(dbbooktable));
 // });
-=======
->>>>>>> bb7e69f8e5c6e1c5dd6b131ad21d31bb6546afe8
 
   app.get("/api/read/:reading_status", (req, res) => {
     db.booktable
@@ -48,21 +45,7 @@ module.exports = function (app) {
       .then((bookread) => res.json(bookread));
   });
 
-<<<<<<< HEAD
 // module.exports = function (app) {};
-=======
-  app.post("/api/books", (req, res) => {
-    db.booktable
-      .create({
-        title: req.body.title,
-        author: req.body.author,
-        user_rating: req.body.user_rating,
-        reading_status: req.body.reading_status,
-        category: req.body.category,
-      })
-      .then((bookadd) => res.json(bookadd));
-  });
->>>>>>> bb7e69f8e5c6e1c5dd6b131ad21d31bb6546afe8
 
   app.delete("/api/books/: title", (req, res) => {
     db.booktable
