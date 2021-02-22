34631ec3363de032595752c783cb5d52cce779dc
amo02008 <adrienne.osorio@gmail.com>
2021-02-22 18:50:17 -0500

    added the word stone to title of book 1

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,7 @@ use bookly_db;
 INSERT INTO booktables (title, author, user_rating, reading_status, category, createdAt, updatedAt) 
 VALUES 
 
-    ("Harry Potter and the Sorcerer's", "J.K. Rowling", "Awesome", "Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
+    ("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Awesome", "Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
     ("Harry Potter and the Chamber of Secrets", "J.K. Rowling", "Awesome", "Reading", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
     ("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "Awesome", "Reading", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
     ("Harry Potter and the Goblet of Fire", "J.K. Rowling", "Not Yet Rated", "To Read", "Fantasy","2020-05-07 15:41:58","2020-05-07 15:41:58"),
